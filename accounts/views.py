e54da9e0168f96370541d40f588b77de1a6131f2
createmycode <lyurim58@gmail.com>
2025-01-06 20:22:53 +0900

    🩹 Fix views

--- accounts/views.py
+++ accounts/views.py
@@ -1,40 +1,44 @@
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.decorators import api_view, permission_classes, authentication_classes
-from rest_framework.response import Response
-from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
+from rest_framework.views import APIView
+from .models import User
+from .serializers import SignupSerializer, UserProfileSerializer, UserUpdateSerializer 
+from django.core.cache import cache
 from django.contrib.auth import authenticate
-from django.contrib.auth import get_user_model
-from .serializers import SignupSerializer, UserProfileSerializer, UserUpdateSerializer
-from rest_framework.decorators import api_view
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
+from rest_framework_simplejwt.tokens import RefreshToken
 from django.http import JsonResponse
-from rest_framework_simplejwt.authentication import JWTAuthentication
 
 
-@api_view(['POST'])
-@permission_classes([])
-@authentication_classes([])
-def signup(request):
-    serializer = SignupSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({
-            'message': '회원가입이 성공적으로 완료되었습니다.'
-        }, status=status.HTTP_201_CREATED)
+
+class SignupView(APIView):
+    '''회원가입'''
+    authentication_classes = [] # 인증 설정 무시
+    permission_classes = []     # 권한 없이 누구나 접근 가능
     
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['POST'])
-@permission_classes([])
-@authentication_classes([])
-def login(request):
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        # 사용자 인증
-        user = authenticate(request, username=username, password=password)
+    def post(self, request):
+        serializer = SignupSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({
+                'message': '회원가입이 완료되었습니다.'
+            }, status=status.HTTP_201_CREATED)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+class LoginView(APIView):
+    '''로그인'''
+    authentication_classes = [] # 인증 설정 무시
+    permission_classes = []     # 권한 없이 누구나 접근 가능
+    
+    def post(self, request):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = User.objects.filter(email=email).first()
+        
+        # 사용자  인증
+        user = authenticate(request, email=email, password=password)
         if user is not None:
             # JWT 토큰 생성
             refresh = RefreshToken.for_user(user)
@@ -45,48 +49,43 @@ def login(request):
             }, status=200)
         else:
             return JsonResponse({'error': '사용자명 또는 비밀번호가 올바르지 않습니다.'}, status=400)
+        
 
-@api_view(['POST'])
-@permission_classes([])
-@authentication_classes([])
-def logout(request):
-    print('---')
-    try:
-        refresh_token = request.data.get("refresh")
-        print(refresh_token)
-        token = RefreshToken(refresh_token)
-        print(token)
-        token.blacklist()
-        print('---')
-        return Response({"message": "로그아웃 성공"})
-    except Exception:
-        return Response({"error": "로그아웃 실패"}, 
-            status=status.HTTP_400_BAD_REQUEST)
-
-
-@api_view(['GET','PUT','PATCH'])
-def profile(request):
-    user = request.user # JWT 인증을 통해 얻은 현재 사용자
-
-    if request.method == 'GET':
-        serializer = UserProfileSerializer(user, context={'request':request})
-        return Response(serializer.data, status=200)
+class LogoutView(APIView):
+    '''로그아웃'''
+    permission_classes = [IsAuthenticated] # 인증된 사용자만 접근 가능
     
-    if request.method in ('PUT','PATCH'):
-        serializer = UserUpdateSerializer(instance=user, data=request.data, partial=True)  # partial=True로 일부 업데이트 허용
-
-        if serializer not in serializer.fields:
+    def post(self, request):
+        try:
+            refresh_token = request.data['refresh']
+            token = RefreshToken(refresh_token)
+            token.blacklist() # 토큰 블랙리스트 추가
+            return Response({'message': '로그아웃 성공'}, status=200)
+        except Exception as e:
+            return Response({'error': '로그아웃 실패'}, status=400)
+        
+class UserProfile(APIView):
+    '''유저 프로필 조회'''
+    permission_classes = [IsAuthenticated] # 인증된 사용자만 접근 가능
+    
+    def get(self, request):
+        user = request.user
+        serializer = UserProfileSerializer(user)
+        return Response(serializer.data)
+    
+    def put(self, request):
+        user = request.user
+        serializer = UserUpdateSerializer(user, data=request.data, partial=True)
+        if not any(field in serializer.validated_data for field in serializer.fields):
             return Response({
-                'message':'이 정보는 수정할 수 없습니다.'
+                'message': '이 정보는 수정할 수 없습니다.'
             }, status=status.HTTP_400_BAD_REQUEST)
 
         if serializer.is_valid():
-            serializer.save()  
+            serializer.save()
             return Response({
-                'message':'회원정보가 성공적으로 수정되었습니다.',
+                'message': '회원정보가 성공적으로 수정되었습니다.',
                 'user': serializer.data
             }, status=status.HTTP_200_OK)
 
-
-        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
