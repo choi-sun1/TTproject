c7aaded3c225fded8d6937514c0a6db31dd36d54
nyum76 <nyum2024@gmail.com>
2025-01-06 16:58:24 +0900

    📝Comment: urls.py 댓글 수정 삭제 기능 설명 추가

--- articles/urls.py
+++ articles/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.ArticleListCreate.as_view(), name='article_list_create'), # 게시글 목록 조회, 생성
     path('<int:articleId>/', views.ArticleDetail.as_view(), name='article_detail'), # 게시글 상세 조회, 수정, 삭제
-    path('<int:articleId>/comments/', views.CommentListCreate.as_view(), name='comments'), # 댓글 조회, 생성
+    path('<int:articleId>/comments/', views.CommentListCreate.as_view(), name='comments'), # 댓글 조회, 생성, 수정, 삭제
     path('<int:articleId>/comments/<int:commentId>/like/', views.CommentLike.as_view(), name='comment_like'), # 댓글 좋아요 기능
 ]
