6aba766e307baddd502689ee783a4d6a735820d6
nyum76 <nyum2024@gmail.com>
2025-01-15 11:05:36 +0900

    🎉 Add: JS 파일들 추가

--- smart_trip/urls.py
+++ smart_trip/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/accounts/', include('accounts.urls')),
-    path('api/articles/', include('articles.urls')),
-    path('api/chatbot/', include('chatbot.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('accounts/', include('accounts.urls')),
+    path('board/', include('board.urls')),
+    path('chatbot/', include('chatbot.urls')),
+    path('stays/', include('stays.urls')),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
